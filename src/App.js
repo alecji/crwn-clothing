3a4f0e61dde9004cb5c4cc6493980d47451447c5
Alec Inthalangsy <19725614+alecji@users.noreply.github.com>
2020-05-07 01:14:05 -0400

    add collection preview

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 import "./App.css";
-
-const Hats = () => {
-  return (
-    <div>
-      <h1>Hats</h1>
-    </div>
-  );
-};
 
 const App = () => {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route exact path="/shop/hats" component={Hats} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
